abd771aa3f6a2a9111adc56c977ce7e09703b025
yaocong <rogwel@vip.qq.com>
2021-03-16 11:42:36 +0800

    add: API, App(entrance), Home(home page), TopBar(Components), Table(Components), Carousel(Components), covid-19(png)

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator'
+import { Vue, Component } from 'vue-property-decorator'
 import { format, subDays } from 'date-fns'
 import { newsCountList } from '@/api/api'
 import * as echarts from 'echarts/core'
